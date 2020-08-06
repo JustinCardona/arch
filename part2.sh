7c04a682f086d6f8c1a1887c5789607512115888
JustinCardona <cardonajustin15@gmail.com>
2020-08-06 13:40:39 -0400

    language problems

--- part2.sh
+++ part2.sh
@@ -29,9 +29,9 @@ done
 # Host configuration
 ln -sf /usr/share/zoneinfo/"$region"/"$zone" /etc/localtime
 hwclock --systohc --utc
-echo "LANG=en_US.UTF-8" > /etc/locale.gen
+echo "en_US.UTF-8" > /etc/locale.gen
 locale-gen
-echo "en_US.UTF-8" > /etc/locale.conf
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
 echo "$host" > /etc/hostname
 echo -e "127.0.0.1\tlocalhost\n::1\t\tlocalhost\n127.0.1.1\t$host.localdomain\t$host"> /etc/hosts
 clear
