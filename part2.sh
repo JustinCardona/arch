3da58dc88c0697f48a6059acb10e7478913c32d1
JustinCardona <cardonajustin15@gmail.com>
2020-10-29 23:27:26 -0400

    fixed locales

--- part2.sh
+++ part2.sh
@@ -29,7 +29,7 @@ done
 # Host configuration
 ln -sf /usr/share/zoneinfo/"$region"/"$zone" /etc/localtime
 hwclock --systohc --utc
-echo "en_US.UTF-8" > /etc/locale.gen
+echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 echo "$host" > /etc/hostname
