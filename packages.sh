266b9ea267ab41532422bcc7ed0ce377b8794d48
JustinCardona <cardonajustin15@gmail.com>
2020-12-10 17:08:32 -0500

    brightness control

--- packages.sh
+++ packages.sh
@@ -6,4 +6,8 @@ sudo pacman -S flameshot nautilus code gimp shotcut blender discord firefox
 yay -S oreo-cursors-git numix-icon-theme-git vertex-themes chili-sddm-theme bitwarden vlc xournal
 read -p "Change the current SDDM theme to chili. Afterwards go to lxappearance to change system themes."
 sudo vim /usr/lib/sddm/sddm.conf.d/sddm.conf
-cp -a PostInstall folder
+cp -a PostInstall ~/.config
+cd /etc
+sudo git clone https://github.com/ChrisTitusTech/firewallsetup.git
+cd firewallsetup
+./firewal
---
+++ updateConfig.sh
@@ -0,0 +1,3 @@
+cp -r ~/.config/i3 PostInstall/
+cp -r ~/.config/polybar PostInstall/
+cp -r ~/.config/scripts PostInstall/
