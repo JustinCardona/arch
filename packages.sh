c461f6e0d60df5b603630c42c19888420027117a
JustinCardona <58197133+JustinCardona@users.noreply.github.com>
2020-12-10 18:30:03 -0500

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -6,6 +6,7 @@ sudo pacman -S flameshot nautilus code gimp shotcut blender discord firefox
 yay -S oreo-cursors-git numix-icon-theme-git vertex-themes chili-sddm-theme bitwarden vlc xournal
 read -p "Change the current SDDM theme to chili. Afterwards go to lxappearance to change system themes."
 sudo vim /usr/lib/sddm/sddm.conf.d/sddm.conf
+cd ~/JustinCardona.github.io
 cp -a PostInstall ~/.config
 cd /etc
 sudo git clone https://github.com/ChrisTitusTech/firewallsetup.git
