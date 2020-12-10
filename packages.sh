b536d28b5c4c4ec57f0658d905cd75af6cfe4d03
JustinCardona <58197133+JustinCardona@users.noreply.github.com>
2020-12-10 18:58:53 -0500

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -2,8 +2,8 @@ cd ~
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si PKGBUILD
-sudo pacman -S flameshot nautilus code gimp shotcut blender discord firefox
-yay -S polybar oreo-cursors-git numix-icon-theme-git vertex-themes chili-sddm-theme bitwarden vlc xournal
+#sudo pacman -S flameshot nautilus code gimp shotcut blender discord firefox
+yay -S polybar #oreo-cursors-git numix-icon-theme-git vertex-themes chili-sddm-theme bitwarden vlc xournal
 read -p "Change the current SDDM theme to chili. Afterwards go to lxappearance to change system themes."
 sudo vim /usr/lib/sddm/sddm.conf.d/sddm.conf
 cd ~/JustinCardona.github.io
