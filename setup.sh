3f1252037ec5c67c52e0452c4275c3abca5d5e0e
JustinCardona <58197133+JustinCardona@users.noreply.github.com>
2020-12-10 17:47:14 -0500

    debugging step

--- setup.sh
+++ setup.sh
@@ -67,7 +67,8 @@ grub-mkconfig -o /boot/grub/grub.cfg
 systemctl enable NetworkManager.service
 systemctl enable sddm.service
 systemctl enable sshd.service
-
+# debugging step
+read -p ""
 # User configuration
 useradd -mG wheel "$name"
 clear
