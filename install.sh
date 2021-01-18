f9144cf8649e598f781d10228bf423287a53245f
JustinCardona <58197133+JustinCardona@users.noreply.github.com>
2021-01-18 12:57:22 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,9 +10,9 @@ do
     break
 done
 wipefs -a "$dev"
-printf "g\nn\n\n\n+256M\nt\n1\nn\n\n\n\nw\n" | sudo fdisk "$dev"
-efip=`lsblk $disk -p -n -l -o NAME -e 7,11 | sed -n 2p`
-rootp=`lsblk $disk -p -n -l -o NAME -e 7,11 | sed -n 3p`
+printf "g\nn\n\n\n+256M\nt\n1\nn\n\n\n\nw\n" | fdisk "$dev"
+efip=`lsblk $dev -p -n -l -o NAME -e 7,11 | sed -n 2p`
+rootp=`lsblk $dev -p -n -l -o NAME -e 7,11 | sed -n 3p`
 mkfs.fat -F32 "$efip"
 mkfs.ext4 "$rootp"
 mount "$rootp" /mnt
