81fd83c72e8e1dd2279f5db254eae598a8a52bb5
JustinCardona <cardonajustin15@gmail.com>
2021-01-18 13:52:21 -0500

    change brave vesion and update confs

--- PostInstall/packages.sh
+++ PostInstall/packages.sh
@@ -5,6 +5,6 @@ cd yay
 makepkg -si PKGBUILD
 
 # Install desktop and applications
-yay -S --noconfirm vulkan-icd-loader lib32-vulkan-icd-loader xorg-fonts-misc sddm i3-wm lxappearance dmenu rofi terminator feh polybar oreo-cursors-git numix-icon-theme-git vertex-themes chili-sddm-theme brave bitwarden discord vlc flameshot nautilus anaconda 
+yay -S --noconfirm vulkan-icd-loader lib32-vulkan-icd-loader xorg-fonts-misc sddm i3-wm lxappearance dmenu rofi terminator feh polybar oreo-cursors-git numix-icon-theme-git vertex-themes chili-sddm-theme brave-bin bitwarden discord vlc flameshot nautilus anaconda 
 
 systemctl enable sddm.service
