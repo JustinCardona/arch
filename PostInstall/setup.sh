7637a93b43db60dbdc670053b0c8064a672c360f
JustinCardona <58197133+JustinCardona@users.noreply.github.com>
2021-02-02 17:43:50 -0500

    Update setup.sh

--- PostInstall/setup.sh
+++ PostInstall/setup.sh
@@ -7,14 +7,10 @@
 #rm -rf paru
 
 # Install desktop and applications
-#paru -S alacritty breeze-default-cursor-theme breeze-gtk feh flameshot lxappearance numlockx qtile xorg-xinit
-paru -S alacritty breeze-default-cursor-theme breeze-gtk feh flameshot lxappearance numlockx pulseaudio spectrwm xorg xorg-fonts-misc xorg-xinit
+paru -S alacritty breeze-default-cursor-theme breeze-gtk feh flameshot lxappearance numlockx pulseaudio xorg xorg-fonts-misc xorg-xinit
 
-# Deploy configs
-mkdir .config
-git clone https://github.com/JustinCardona/JustinCardona.github.io.git
-cd JustinCardona.github.io/PostInstall/
-sh deployConfig.sh
-cd ~
-rm -rf JustinCardona.github.io
-startx
+# Configuration
+cp /etc/X11/xinit/xinitrc ~/.xinitrc
+sed -n -e :a -e '1,5!{P;N;D;};N;ba'
+echo "exec i3" >> ~/.xinitrc
+cp /etc/i3/config ~/.config/i3/
