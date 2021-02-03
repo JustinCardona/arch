e714de42d3a24da2e9f85e427542fbe417b91446
JustinCardona <cardonajustin15@gmail.com>
2021-02-02 19:37:19 -0500

    fixing

--- PostInstall/setup.sh
+++ PostInstall/setup.sh
@@ -7,7 +7,7 @@
 #rm -rf paru
 
 # Install desktop and applications
-paru -S alacritty breeze-default-cursor-theme breeze-gtk feh flameshot i3-gaps lxappearance numlockx pulseaudio rofi xorg xorg-fonts-misc xorg-xinit
+paru -S alacritty breeze-default-cursor-theme breeze-gtk feh flameshot i3-gaps lxappearance numlockx polybar pulseaudio rofi xorg xorg-fonts-misc xorg-xinit
 
 # Configuration
 git clone https://github.com/JustinCardona/JustinCardona.github.io.git
@@ -16,6 +16,7 @@ cp -r config/. ~
 cp -r i3 ~/.config
 cp -r polybar ~/.config
 cp -r scripts ~/.config
-cd
+cd ~
 rm -rf JustinCardona.github.io/PostInstall
+chmod +x ~/.config/polybar/launch.sh
 
