8f539feee10da71d139627e587b30e079c384090
JustinCardona <58197133+JustinCardona@users.noreply.github.com>
2021-02-07 20:05:38 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ mkdir /mnt/boot
 mount "$efip" /mnt/boot
 
 # Installation
-pacstrap /mnt base base-devel efibootmgr git grub linux linux-firmware linux-headers networkmanager sudo vim
+pacstrap /mnt base base-devel efibootmgr git grub linux linux-firmware linux-headers networkmanager sudo vim pulseaudio xorg
 genfstab -U /mnt >> /mnt/etc/fstab
 curl -L JustinCardona.github.io/chroot.sh > chroot.sh
 mv chroot.sh /mnt
