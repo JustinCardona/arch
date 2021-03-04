fd51e7678a8f53a819cbe3a12b02d22d558d89bf
JustinCardona <58197133+JustinCardona@users.noreply.github.com>
2021-03-04 16:33:59 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -60,55 +60,12 @@ pacstrap /mnt $packages
 genfstab -U /mnt >> /mnt/etc/fstab
 
 # Chroot
-chroot_func()
-{
-	# User information
-	clear
-	read -p "Enter the host name: " host
-	clear
-	read -p "Enter the user name: " name
-	clear
-	read -p "Enter the region: " region
-	clear
-	read -p "Enter the zone: " zone
-
-	# Host configuration
-	ln -sf /usr/share/zoneinfo/"$region"/"$zone" /etc/localtime
-	hwclock --systohc --utc
-	echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
-	locale-gen
-	echo "LANG=en_US.UTF-8" > /etc/locale.conf
-	echo "$host" > /etc/hostname
-	echo -e "127.0.0.1\tlocalhost\n::1\t\tlocalhost\n127.0.1.1\t$host.localdomain\t$host"> /etc/hosts
-
-	# Grub configuration
-	if [ $boot_state -gt 0 ]
-	then
-		grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
-	else
-		grub-install --target=i386-pc "$rootp"
-	fi
-	grub-mkconfig -o /boot/grub/grub.cfg
-
-	# Enable services
-	systemctl enable NetworkManager.service
-	systemctl enable iptables.service
-	systemctl enable systemd-timesyncd.service
-
-	# User configuration
-	useradd -mG wheel "$name"
-	clear
-	echo "Set a password for your user"
-	passwd "$name"
-	clear
-	echo "Set a password for the root user (admin)"
-	passwd
-	echo "%wheel ALL=(ALL) ALL" > /etc/sudoers
-}
-export -f chroot_func
-chroot /mnt sh "chroot_func"
-
-sed -i '1itask goes here' todo.txt
+#Chroot
+curl -L JustinCardona.github.io/test_chroot.sh > chroot.sh
+sed -i '1irootp="$rootp"' chroot.sh
+mv chroot.sh /mnt
+arch-chroot /mnt sh chroot.sh
+rm /mnt/chroot.sh
 #umount -a
 #reboot
 
