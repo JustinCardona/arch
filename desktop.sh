df4189fa651e56cdf42505433d523cdd789380a1
JustinCardona <58197133+JustinCardona@users.noreply.github.com>
2021-03-07 21:34:20 -0500

    Update desktop.sh

--- desktop.sh
+++ desktop.sh
@@ -6,4 +6,7 @@ cd ..
 rm -rf yay
 
 # Install packages for desktop environment
-yay -S i3-gaps polybar rofi
+yay -S chromium flameshot i3-gaps picom polybar rofi sddm terminator
+
+# Enable Services
+systemctl enable sddm.service
